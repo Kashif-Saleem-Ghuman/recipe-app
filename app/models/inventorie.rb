8c5e4c13a320ce1e5a1a4beccbfcf0880aa8ba37
Saeqa Sultani <saeqasultani@gmail.com>
2023-04-18 21:39:10 +0430

    Fix linters

--- app/models/inventorie.rb
+++ app/models/inventorie.rb
@@ -1,4 +1,4 @@
 class Inventorie < ApplicationRecord
-    belongs_to :user, class_name: 'User', foreign_key: 'user_id'
-    has_many :inventorie_foods
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  has_many :inventorie_foods
 end
