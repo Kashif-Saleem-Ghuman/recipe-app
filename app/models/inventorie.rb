0fc21cadf657605d06b12b288703ca150428a78d
Saeqa Sultani <saeqasultani@gmail.com>
2023-04-18 21:35:39 +0430

    Create modles for inventorie and inventorie_foods

---
+++ app/models/inventorie.rb
@@ -0,0 +1,4 @@
+class Inventorie < ApplicationRecord
+    belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+    has_many :inventorie_foods
+end
---
+++ app/models/inventory_food.rb
@@ -0,0 +1,4 @@
+class InventoryFood < ApplicationRecord
+    belongs_to :inventorie, class_name: 'Inventorie', foreign_key: 'inventorie_id'
+    belongs_to :food, class_name: 'Food', foreign_key: 'food_id'
+end
