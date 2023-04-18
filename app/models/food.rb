673e31eff5f701fe751d9884e74eaac85ca8a95a
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-04-18 18:22:32 +0500

    Add models
    In this commit
    - Add models for food, recipe, recipe_food and the user.

---
+++ app/models/food.rb
@@ -0,0 +1,5 @@
+class Food < ApplicationRecord
+  has_many :recipe_foods
+  has_many :inventory_foods
+  has_many :recipes, through: :recipe_foods
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :recipes
   has_many :inventories
+
+  validates :name, presence: true 
 end
