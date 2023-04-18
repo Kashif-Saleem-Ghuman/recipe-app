01934b291f01f617937901f0d98556a87ad5bf5d
Saeqa Sultani <saeqasultani@gmail.com>
2023-04-18 21:46:39 +0430

    Fix the spelling issue

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,4 +1,4 @@
-class Inventorie < ApplicationRecord
+class Inventory < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
-  has_many :inventorie_foods
+  has_many :inventory_foods
 end
--- app/models/inventory_food.rb
+++ app/models/inventory_food.rb
@@ -1,4 +1,4 @@
 class InventoryFood < ApplicationRecord
-  belongs_to :inventorie, class_name: 'Inventorie', foreign_key: 'inventorie_id'
+  belongs_to :inventory, class_name: 'Inventory', foreign_key: 'inventory_id'
   belongs_to :food, class_name: 'Food', foreign_key: 'food_id'
 end
