3ebbca62a7b2a241119c1e5eae57ed4ff0f7e221
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-04-19 20:32:20 +0500

    Update foods_controller.rb
    Update the crate method for the Food.

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -10,7 +10,7 @@ class FoodsController < ApplicationController
   end
 
   def create
-    @food = current_user.foods.new(food_params)
+    @food = current_user.foods.create(food_params)
 
     if @food.save
       redirect_to foods_path, notice: 'Your Food is created successfully 💯'
