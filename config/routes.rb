bba886638229d5dafc71d65203b4984f4ce0b070
Saeqa Sultani <saeqasultani@gmail.com>
2023-04-19 23:13:10 +0430

    Create controllers for Inventories and InventoryFoods

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+ 
   devise_for :users
+  resources :inventories, only: [:index, :show, :new, :create, :destroy] do
+    resources :inventory_foods
+  end
   resources :foods, only: [:index, :new, :create, :destroy]
   root "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
