468a177839e55ff797da9aa9133635c7f69ce86f
unknown <kashif_saleem_23@hotmail.com>
2023-04-20 16:47:40 +0500

    Correct column name to quantity in recipe_foods table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_19_151248) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_20_103156) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,7 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_19_151248) do
   end
 
   create_table "recipe_foods", force: :cascade do |t|
-    t.integer "qunatity"
+    t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "food_id", null: false
