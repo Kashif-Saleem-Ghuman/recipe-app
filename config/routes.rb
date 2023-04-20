d151586b09d290bbd38d0c515737b7223aca7c71
Saeqa Sultani <saeqasultani@gmail.com>
2023-04-20 13:19:09 +0430

    Add create action and destroy action to the inventories_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
  
   devise_for :users
-  resources :inventories, only: [:index, :show, :new, :create, :destroy] do
-    resources :inventory_foods
-  end
+  resources :inventories, only: [:index, :show, :new, :create, :destroy]
+ 
   resources :foods, only: [:index, :new, :create, :destroy]
   root "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
