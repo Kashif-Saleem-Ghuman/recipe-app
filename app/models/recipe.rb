b82c670d2248c1cc9bfa58906814288cc200e203
unknown <kashif_saleem_23@hotmail.com>
2023-04-20 16:44:37 +0500

    Add association with foods and add totol price function

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,12 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
+  has_many :foods, through: :recipe_foods
+
 
   validates :name, presence: true
+
+  def total_price
+    foods.map { |f| f.price * f.recipe_foods.find_by(recipe_id: id).quantity }.sum
+  end
 end
