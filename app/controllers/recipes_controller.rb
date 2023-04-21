19b934491114530fb70fa945c5c605303fde85f2
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-04-21 17:43:33 +0500

    Add generate shopping list popup in recipes
    In this commit
    - Add a pop up when user clicks generate shopping list.
    - From the pop up when the user clicks 'generate', it redirects the user to the shopping list page.
    - Edit routes to match the new functionalities.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,6 +9,7 @@ class RecipesController < ApplicationController
     @current_user = current_user
     @recipe = Recipe.find_by_id(params[:id])
     @ingredients = @recipe&.recipe_foods
+    @inventories = Inventory.all
   end
 
   def new
@@ -43,6 +44,40 @@ class RecipesController < ApplicationController
     @recipe.public = !@recipe.public
 
     redirect_to recipe_path(@recipe.id), notice: 'Recipe updated successfully 👏🏻' if @recipe.save
+  end
+
+  def generate_list
+    redirect_to shopping_list_path(params[:recipe_id], params[:inventory])
+  end
+
+  def generate
+    @quantity = []
+    @foods = []
+    @recipe_food = RecipeFood.where(recipe_id: params[:recipe_id])
+    @inventory_food = InventoryFood.where(inventory_id: params[:inventory_id])
+    @recipe_food.each do |rf|
+      found = false
+      check_recipe_food = Food.find(rf.food_id)
+      @inventory_food.each do |each|
+        check_inventory_food = Food.find(each.food_id)
+        next unless check_inventory_food.name == check_recipe_food.name
+
+        found = true
+        required = rf.quantity - each.quantity
+        if required.positive?
+          @quantity << [required, check_inventory_food.price]
+          @foods << check_inventory_food.name
+        end
+      end
+      next if found
+
+      @quantity << [rf.quantity, check_recipe_food.price]
+      @foods << check_recipe_food.name
+    end
+    @total = 0
+    @quantity.each do |q|
+      @total += q[0].to_i * q[1].to_i
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,14 @@ Rails.application.routes.draw do
   resources :foods, only: [:index, :new, :create, :destroy]
   resources :recipes, only: [:index, :show, :destroy, :new, :create] do
      resources :recipe_foods, only: [:new, :create, :destroy]
+     resources :general_shopping_list, only: [:index ,:show]
   end
   get 'public_recipes', to: 'recipes#public_recipes'
-  get 'shopping_list', to: 'shopping_list#index'
+  post 'shopping_list/recipe_id=:recipe_id', to: 'recipes#generate_list', as: 'generate_shopping_list'
+  get 'shopping_list/recipe_id=:recipe_id&inventory_id=:inventory_id', to: 'recipes#generate', as: "shopping_list"
 
   match 'recipes/:recipe_id' => 'recipes#toggle_public', as: :toggle_public, via: :patch
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
